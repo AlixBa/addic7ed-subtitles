d373d4b6075fc9a8fd07b2b10f2ac56d98c2a9d1
Martin Lindhe <martin@ubique.se>
2015-03-25 13:20:23 +0100

    tests: give tests more descriptive names

--- test/AlixBa/Addic7edSubtitles/Helpers/EpisodeTest.php
+++ test/AlixBa/Addic7edSubtitles/Helpers/EpisodeTest.php
@@ -1,12 +1,10 @@
 <?php
 
-// XXX assuming input name is: The.Big.Bang.Theory.S08E18.HDTV.x264-LOL.srt, what is show name?!
-
 use AlixBa\Addic7edSubtitles\Helpers\Episode;
 
 class EpisodeTest extends \PHPUnit_Framework_TestCase
 {
-    function testOne()
+    function testCapitalizedFileName()
     {
         $episode = new Episode('The.Big.Bang.Theory.S08E18.HDTV.x264-LOL.mp4');
 
@@ -18,8 +16,7 @@ class EpisodeTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(['hdtv', 'x264'], $episode->tags);
     }
 
-
-    function testTwo()
+    function testLowerCasedFileName()
     {
         $episode = new Episode('the.big.bang.theory.818.hdtv-lol.mp4');
 
