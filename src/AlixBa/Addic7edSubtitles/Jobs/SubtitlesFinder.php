59f6c0b403ef86252ba6524f7cb33caed0ca8042
AlixBa <alix.baggio@outlook.fr>
2015-03-26 15:52:44 +0100

    fixes bad parameter

--- src/AlixBa/Addic7edSubtitles/Jobs/SubtitlesFinder.php
+++ src/AlixBa/Addic7edSubtitles/Jobs/SubtitlesFinder.php
@@ -72,12 +72,12 @@ final class SubtitlesFinder
         $languageId = $this->languages[$languageId];
 
         $episode = new Episode($episodeFilename);
-        if (!isset($this->shows[$episode->showName])) {
+        if (!isset($this->shows[$episode->sanitizedShowName])) {
             printf("Missing show [%s].\n", $episode->showName);
             return null;
         }
 
-        $showId = $this->shows[$episode->showName];
+        $showId = $this->shows[$episode->sanitizedShowName];
         $url    = $this->builder->getAddictedShowAjaxUrl($showId, $episode->season, $languageId);
 
         printf("Trying to get subtitles from %s.\n", $url);
